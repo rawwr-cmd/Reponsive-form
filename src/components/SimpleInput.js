0d39047ee125bfe8c36b8b530c2bac8319fd0ea8
rawwr <akshayace0302@gmail.com>
2022-06-27 21:05:48 +0530

    managing the overall form in a effective way

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,20 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      setFormIsValid(true);
-    } else {
-      setFormIsValid(false);
-    }
-  }, [enteredNameIsValid]);
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
